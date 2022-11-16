83c435225870a79684983a74c91940d4bef80d68
annasitailo <sitajloanna@gmail.com>
2022-11-16 18:18:29 +0200

    Provided implementation

--- sem_1_lab_1/main.py
+++ sem_1_lab_1/main.py
@@ -1,23 +1,45 @@
 class Directory:
 
     def __init__(self):
-        self.directories = []
-        self.files = []
+        self.parent = None
+        self.name = "root"
+        self.MAX_DIR_SIZE = 10
+        self.children = []
 
-    def create(self, path: str):
-        pass
+    def create(self, parent, name, max_size):
+        self.name = name
+        self.MAX_DIR_SIZE = max_size
+        if parent != None:
+            if len(parent.children) >= parent.MAX_DIR_SIZE:
+                print(parent.name + " is full!")
+                return
+            self.parent = parent
+            self.parent.children.append(self)
+            print(self.name + " was created successfully!")
+            return
 
-    def delete(self, path: str):
-        pass
-
-    def move(self, path: str):
-        pass
+    def move(self, new_parent):
+        if new_parent != None:
+            if len(new_parent.children) >= new_parent.MAX_DIR_SIZE:
+                print(new_parent.name + " is full! Cannot move " + self.name)
+                return
+            if self.parent != None:
+                self.parent.children.remove(self)
+            self.parent = new_parent
+            self.parent.children.append(self)
+            print(self.name + " was moved to " + self.parent.name + " successfully!")
+            return
+        print(new_parent + " is empty!")
+    
+    def __delete__(self):
+        self.parent.children.remove(self)
+        print(self.name + " was deleted successfully!")
 
 
 class File:
 
-    def __init__(self, path):
-        self.filepath = path
+    def __init__(self):
+        pass
 
     def create(self):
         pass
@@ -31,83 +53,135 @@ class File:
 
 class BinaryFile(File):
 
-    def __init__(self, path):
-        File.__init__(self, path)
+    def __init__(self):
+        File.__init__(self)
+        self.parent = None
+        self.name = "file"
+        self.content = ""
 
-    def create(self):
-        pass
+    def create(self, parent, name, content):
+        # if parent == None:
+        #     print(parent.name + " was not set!")
+        #     return
+        if len(parent.children) >= parent.MAX_DIR_SIZE:
+            print(parent.name + " is full!")
+            return
+        self.parent = parent
+        self.name = name
+        self.content = content
+        self.parent.children.append(self)
+        print(self.name + " was created successfully!") 
 
-    def delete(self):
-        pass
+    def __delete__(self):
+        self.parent.children.remove(self)
+        print(self.name + " was deleted successfully!")
 
-    def move(self):
-        pass
+    def move(self, new_parent):
+        if new_parent != None:
+            if len(new_parent.children) >= new_parent.MAX_DIR_SIZE:
+                print(new_parent.name + " is full! Cannot move " + self.name)
+                return
+            if self.parent != None:
+                self.parent.children.remove(self)
+            self.parent = new_parent
+            self.parent.children.append(self)
+            print(self.name + " was moved to " + self.parent.name + " successfully!")
+            return
+        print(new_parent + " is empty!")
 
     def read_file(self):
-        return "file content"
+        print(self.content)
+        return self.content
 
 
 class LogTextFile(File):
 
-    def __init__(self, path):
-        File.__init__(self, path)
+    def __init__(self):
+        File.__init__(self)
+        self.parent = None
+        self.name = "file"
+        self.content = []
 
-    def create(self):
-        pass
+    def create(self, parent, name, content):
+        if len(parent.children) >= parent.MAX_DIR_SIZE:
+            print(parent.name + " is full!")
+            return
+        self.parent = parent
+        self.name = name
+        self.content.append(content)
+        self.parent.children.append(self)
+        print(self.name + " was created successfully!")
 
-    def delete(self):
-        pass
+    def __delete__(self):
+        self.parent.children.remove(self)
+        print(self.name + " was deleted successfully!")
 
-    def move(self):
-        pass
+    def move(self, new_parent):
+        if new_parent != None:
+            if len(new_parent.children) >= new_parent.MAX_DIR_SIZE:
+                print(new_parent.name + " is full! Cannot move " + self.name)
+                return
+            if self.parent != None:
+                self.parent.children.remove(self)
+            self.parent = new_parent
+            self.parent.children.append(self)
+            print(self.name + " was moved to " + self.parent.name + " successfully!")
+            return
+        print(new_parent + " is empty!")
 
     def read_file(self):
-        return "file content"
+        for i in self.content:
+            print(i)
+        return self.content
 
-    def append_line(self):
-        pass
+    def append_line(self, line):
+        self.content.append(line)
+        print("Line was added successfully!")
 
 
 class BufferFile(File):
 
-    MAX_BUF_FILE_SIZE = 1024
+    def __init__(self):
+        File.__init__(self)
+        self.parent = None
+        self.name = "file"
+        self.MAX_BUF_FILE_SIZE = 10
+        self.content = []
 
-    def __init__(self, path):
-        File.__init__(self, path)
+    def create(self, parent, name, max_size):
+        if len(parent.children) >= parent.MAX_DIR_SIZE:
+            print(parent.name + " is full!")
+            return
+        self.parent = parent
+        self.name = name
+        self.MAX_BUF_FILE_SIZE = max_size
+        self.parent.children.append(self)
+        print(self.name + " was created successfully!")
 
-    def create(self):
-        pass
+    def __delete__(self):
+        self.parent.children.remove(self)
+        print(self.name + " was deleted successfully!")
 
-    def delete(self):
-        pass
+    def move(self, new_parent):
+        if new_parent != None:
+            if len(new_parent.children) >= new_parent.MAX_DIR_SIZE:
+                print(new_parent.name + " is full! Cannot move " + self.name)
+                return
+            if self.parent != None:
+                self.parent.children.remove(self)
+            self.parent = new_parent
+            self.parent.children.append(self)
+            print(self.name + " was moved to " + self.parent.name + " successfully!")
+            return
+        print(new_parent + " is empty!")
 
-    def move(self):
-        pass
-
-    def push(self, element: str):
-        pass
+    def push(self, element):
+        if len(self.content) >= self.MAX_BUF_FILE_SIZE:
+            print("The storage is full!")
+            return
+        self.content.append(element)
 
     def consume(self):
-        pass
-
-
-class FileSystem:
-
-    def init(self):
-        self.directories = []
-        self.files = []
-
-    def add_directory(self, directory: Directory):
-        pass
-
-    def delete_directory(self, path: str):
-        pass
-    
-    def add_file(self, file: File):
-        pass
-
-    def delete_file(self, path: str):
-        pass
-
-    def get_dir_by_path(self, path: str):
-        pass
+        if len(self.content) != 0:
+            self.content.pop()
+        print("Deleted successfully!")
--- sem_1_lab_1/tests.py
+++ sem_1_lab_1/tests.py
@@ -1,112 +1,116 @@
-from main import FileSystem
-from main import File
 from main import Directory
 from main import BinaryFile
 from main import LogTextFile
 from main import BufferFile
-import pytest
+# # import pytest
 
-class Tests:
+# class Test:
 
-    def test_directory_create():
-        directory = Directory("", "d1", 15)
-        assert directory.parent == ""
-        assert directory.name == "d1"
-        assert directory.MAX_SIZE == 15
+# def helloworld():
+#     return "hello world!"
 
-    def test_directory_move():
-        directory = Directory("", "d1", 15)
-        parent_directory = Directory("", "parent_directory", 25)
-        directory.move(parent_directory)
-        assert directory.parent == parent_directory
+# def test_helloworld():
+#     assert helloworld() == "hello world!"
 
-    def test_directory_delete():
-        parent_directory = Directory("", "parent_directory", 25)
-        directory = Directory(parent_directory, "d1", 15)
-        length = parent_directory.len()
-        del directory
-        assert length - parent_directory.len == 1
-        assert 'directory' not in locals()
+def test_directory_create():
+    directory = Directory("", "d1", 15)
+    assert directory.parent == ""
+    assert directory.name == "d1"
+    assert directory.MAX_SIZE == 15
 
-    def test_buffer_file_create():
-        parent_directory = Directory("", "parent_directory", 25)
-        buffer_file = BufferFile(parent_directory, "buffer_file", 15)
-        assert buffer_file.name == "buffer_file"
-        assert buffer_file.parent == parent_directory
-        assert buffer_file.MAX_BUF_FILE_SIZE == 15
+def test_directory_move():
+    directory = Directory("", "d1", 15)
+    parent_directory = Directory("", "parent_directory", 25)
+    directory.move(parent_directory)
+    assert directory.parent == parent_directory
 
-    def test_buffer_file_move():
-        parent_directory = Directory("", "parent_directory", 25)
-        buffer_file = BufferFile(parent_directory, "buffer_file", 15)
-        new_parent_directory = Directory("", "new", 20)
-        assert buffer_file.parent == new_parent_directory
+def test_directory_delete():
+    parent_directory = Directory("", "parent_directory", 25)
+    directory = Directory(parent_directory, "d1", 15)
+    length = parent_directory.len()
+    del directory
+    assert length - parent_directory.len == 1
+    assert 'directory' not in locals()
 
-    def test_buffer_file_push():
-        parent_directory = Directory("", "parent_directory", 25)
-        buffer_file = BufferFile(parent_directory, "buffer_file", 15)
-        with pytest.raises(OverflowError):
-            for _ in range(16):
-                buffer_file.push("element to push")
+def test_buffer_file_create():
+    parent_directory = Directory("", "parent_directory", 25)
+    buffer_file = BufferFile(parent_directory, "buffer_file", 15)
+    assert buffer_file.name == "buffer_file"
+    assert buffer_file.parent == parent_directory
+    assert buffer_file.MAX_BUF_FILE_SIZE == 15
 
-    def test_buffer_file_consume():
-        parent_directory = Directory("", "parent_directory", 25)
-        buffer_file = BufferFile(parent_directory, "buffer_file", 15)
-        length = buffer_file.len()
-        buffer_file.consume("element to consume")
-        assert length - buffer_file.len() == 1
+def test_buffer_file_move():
+    parent_directory = Directory("", "parent_directory", 25)
+    buffer_file = BufferFile(parent_directory, "buffer_file", 15)
+    new_parent_directory = Directory("", "new", 20)
+    assert buffer_file.parent == new_parent_directory
 
-    def test_buffer_file_delete():
-        parent_directory = Directory("", "parent_directory", 25)
-        buffer_file = BufferFile(parent_directory, "buffer_file", 15)
-        del buffer_file
-        assert 'buffer_file' not in locals()
+# def test_buffer_file_push():
+#     parent_directory = Directory("", "parent_directory", 25)
+#     buffer_file = BufferFile(parent_directory, "buffer_file", 15)
+#     with pytest.raises(OverflowError):
+#         for _ in range(16):
+#             buffer_file.push("element to push")
 
-    def test_log_text_file_create():
-        parent_directory = Directory("", "parent_directory", 25)
-        log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
-        assert log_text_file.name == "log_text_file"
-        assert log_text_file.parent == parent_directory
-        assert log_text_file.content == "content"
+def test_buffer_file_consume():
+    parent_directory = Directory("", "parent_directory", 25)
+    buffer_file = BufferFile(parent_directory, "buffer_file", 15)
+    length = buffer_file.len()
+    buffer_file.consume("element to consume")
+    assert length - buffer_file.len() == 1
 
-    def test_log_text_file_move():
-        parent_directory = Directory("", "parent_directory", 25)
-        log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
-        new_parent_directory = Directory("", "new", 20)
-        assert log_text_file.parent == new_parent_directory
+def test_buffer_file_delete():
+    parent_directory = Directory("", "parent_directory", 25)
+    buffer_file = BufferFile(parent_directory, "buffer_file", 15)
+    del buffer_file
+    assert 'buffer_file' not in locals()
 
-    def test_log_text_file_append_and_read():
-        parent_directory = Directory("", "parent_directory", 25)
-        log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
-        assert log_text_file.read_file() == "content"
-        log_text_file.append_line("something to add")
-        assert log_text_file.read_file() == "content\nsomething to add"
+def test_log_text_file_create():
+    parent_directory = Directory("", "parent_directory", 25)
+    log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
+    assert log_text_file.name == "log_text_file"
+    assert log_text_file.parent == parent_directory
+    assert log_text_file.content == "content"
 
-    def test_log_text_file_delete():
-        parent_directory = Directory("", "parent_directory", 25)
-        log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
-        del log_text_file
-        assert 'log_text_file' not in locals()
+def test_log_text_file_move():
+    parent_directory = Directory("", "parent_directory", 25)
+    log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
+    new_parent_directory = Directory("", "new", 20)
+    assert log_text_file.parent == new_parent_directory
 
-    def test_binary_file_create():
-        parent_directory = Directory("", "parent_directory", 25)
-        binary_file = BinaryFile(parent_directory, "binary_file", "content")
-        assert binary_file.name == "binary_file"
-        assert binary_file.parent == parent_directory
-        assert binary_file.content == "content"
+def test_log_text_file_append_and_read():
+    parent_directory = Directory("", "parent_directory", 25)
+    log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
+    assert log_text_file.read_file() == "content"
+    log_text_file.append_line("something to add")
+    assert log_text_file.read_file() == "content\nsomething to add"
 
-    def test_binary_file_move():
-        parent_directory = Directory("", "parent_directory", 25)
-        binary_file = BinaryFile(parent_directory, "binary_file", "content")
-        new_parent_directory = Directory("", "new", 20)
-        assert binary_file.parent == new_parent_directory
+def test_log_text_file_delete():
+    parent_directory = Directory("", "parent_directory", 25)
+    log_text_file = LogTextFile(parent_directory, "log_text_file", "content")
+    del log_text_file
+    assert 'log_text_file' not in locals()
 
-    def test_binary_file_append_and_read():
-        parent_directory = Directory("", "parent_directory", 25)
-        binary_file = BinaryFile(parent_directory, "binary_file", "content")
-        assert binary_file.read_file() == "content"
+def test_binary_file_create():
+    parent_directory = Directory("", "parent_directory", 25)
+    binary_file = BinaryFile(parent_directory, "binary_file", "content")
+    assert binary_file.name == "binary_file"
+    assert binary_file.parent == parent_directory
+    assert binary_file.content == "content"
 
-    def test_binary_file_delete():
-        parent_directory = Directory("", "parent_directory", 25)
-        binary_file = BinaryFile(parent_directory, "binary_file", "content")
-        del binary_file
-        assert 'binary_file' not in locals()
+def test_binary_file_move():
+    parent_directory = Directory("", "parent_directory", 25)
+    binary_file = BinaryFile(parent_directory, "binary_file", "content")
+    new_parent_directory = Directory("", "new", 20)
+    assert binary_file.parent == new_parent_directory
+
+def test_binary_file_append_and_read():
+    parent_directory = Directory("", "parent_directory", 25)
+    binary_file = BinaryFile(parent_directory, "binary_file", "content")
+    assert binary_file.read_file() == "content"
+
+def test_binary_file_delete():
+    parent_directory = Directory("", "parent_directory", 25)
+    binary_file = BinaryFile(parent_directory, "binary_file", "content")
+    del binary_file
+    assert 'binary_file' not in locals()
